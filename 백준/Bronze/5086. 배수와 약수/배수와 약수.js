c411f39c1a1f94b7ae8cbf40ef39a2b7cf495089
라몽이 <rlatndus21049@gmail.com>
2025-08-17 13:20:02 +0900

    [Bronze III] Title: 배수와 약수, Time: 92 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,18 @@
+// 입력을 한 줄씩 읽어와 배열로 저장
+//const input = require("fs").readFileSync("input.txt", "utf-8").trim().split("\n");
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+  
+let result = [];
+for (let i = 0; i < input.length; i++) {
+  const [a, b] = input[i].trim().split(" ").map(Number);
+  if (a === 0 && b === 0) break;
+
+  if (b % a === 0) {
+    result.push('factor');
+  } else if (a % b === 0) {
+    result.push('multiple');
+  } else {
+    result.push('neither');
+  }
+}
+console.log(result.join("\n"));
