4331e4e85ba37bccf8729e452aa20a5437b67266
라몽이 <rlatndus21049@gmail.com>
2025-09-27 10:46:09 +0900

    [Silver IV] Title: 카드, Time: 296 ms, Memory: 53988 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.js
+++ 백준/Silver/11652. 카드/카드.js
@@ -1,14 +1,36 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = Number(input[0]);
-const cardMap = new Map();
+// https://www.acmicpc.net/problem/11652
+// 11652번: 카드 - 실버4
 
-for (let i = 1; i <= N; i++) {
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const totalLength = Number(input[0]); // 총 카드 수 = 6
+// const cards = input.slice(1).map((v) => Number(v)); // 숫자로 변환 -> [ 1, 2, 1, 2, 1, 2 ]
+
+const cardMap = new Map(); // Map(3) { 1 => 3, 2 => 3 }
+
+for (let i = 1; i <= totalLength; i++) {
   const card = input[i]; // 문자열로 처리
-  cardMap.set(card, (cardMap.get(card) || 0) + 1);
+  if (cardMap.has(card)) {
+    cardMap.set(card, cardMap.get(card) + 1); // 이미 등장한 카드면 개수 +1
+  } else {
+    cardMap.set(card, 1); // 처음 등장하는 카드면 1로 초기화
+  }
 }
 
+// console.log(cardMap); //Map(2) { 1 => 3, 2 => 3 }
+
+// 이제 정렬을 통해 가장 많이 등장한 카드 찾아 정렬하기
 const sorted = [...cardMap.entries()].sort(
-  (a, b) => b[1] - a[1] || (BigInt(a[0]) < BigInt(b[0]) ? -1 : 1)
+  (a, b) => b[1] - a[1] || (BigInt(a[0]) < BigInt(b[0]) ? -1 : 1) // 등장횟수 내림차순, 같으면 카드번호 오름차순 -> BigInt로 변환 후 비교 (카드 번호가 10억까지 가능하기 때문)
 );
 
-console.log(sorted[0][0]);
+console.log(sorted[0][0]); // 제일 앞에 있는 카드의 숫자 출력
+
+// input example
+// 6
+// 1
+// 2
+// 1
+// 2
+// 1
+// 2
