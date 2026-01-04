43eeffc7c3abf8ddaaef68f23033de8aa0c6696a
라몽이 <rlatndus21049@gmail.com>
2026-01-04 13:42:08 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -0,0 +1,13 @@
+function solution(numbers, num1, num2) {
+    var answer = [];
+    
+    return numbers.slice(num1,num2+1);
+}
+
+//function solution(numbers, num1, num2) {
+//    var answer = [];
+//    for(let i = num1; i <= num2; i++){
+//        answer.push(numbers[i]);
+//    }
+//    return answer;
+//}
