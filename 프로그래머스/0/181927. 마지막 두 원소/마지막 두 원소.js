389fc2022241679ac06a04896a54307c0e80d666
라몽이 <rlatndus21049@gmail.com>
2026-01-20 12:29:23 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [...num_list];
+    
+    if(num_list[num_list.length-1] > num_list[num_list.length-2]){
+        answer.push(num_list[num_list.length-1] - num_list[num_list.length-2])
+    } else {
+        answer.push(num_list[num_list.length-1] * 2)
+    }
+    return answer;
+}
