d381c29438d1e939a28409999f29bdf13c7e4df9
라몽이 <rlatndus21049@gmail.com>
2026-01-26 21:18:37 +0900

    [level 0] Title: 다항식 더하기, Time: 3.61 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,28 +1,9 @@
 function solution(polynomial) {
- 
-    let sumX = 0;
-    let sumNumber = 0
-    const newPolynomial = polynomial.split(" ");
-
-    for(let i =0; i<newPolynomial.length; i++){
-        if(newPolynomial[i] === '+') continue;
-        
-        if (newPolynomial[i].includes("x")) {
-            if (newPolynomial[i] === "x") {
-                sumX = sumX +1;
-            } else {
-                sumX = sumX + Number(newPolynomial[i].replace('x',''))
-            }
-        }else {
-            sumNumber =sumNumber + Number(newPolynomial[i])
-        }
+    var answer = '';
+    polynomial.split(' ')
+    console.log(polynomial)
+    for(let i =0; i<polynomial.length; i++){
+        console.log(polynomial[i])
     }
-    
-
-    sumX = sumX === 0 ? '' : (sumX === 1 ? 'x' : sumX + 'x');
-    sumNumber = sumNumber === 0 ? '' : (sumX ? ' + ' + sumNumber : String(sumNumber));
-    console.log(sumX, sumNumber)
-    let result = sumX + sumNumber
-    
-    return String(result);
+    return answer;
 }
