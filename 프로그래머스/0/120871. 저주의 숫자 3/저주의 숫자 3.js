23ed43dd737ccf303af932e992b00c1d7fd14b44
라몽이 <rlatndus21049@gmail.com>
2026-01-28 21:54:46 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let count = 0;   
+    let num = 0;    
+
+    while (count < n) {
+        num++;
+
+        if (num % 3 === 0 || String(num).includes('3')) {
+            continue; 
+        }
+
+        count++; 
+    }
+
+    return num;
+}
