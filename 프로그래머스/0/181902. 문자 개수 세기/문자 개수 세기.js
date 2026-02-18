7d977476771c3cb6ded8ff7d7c31fce8614f4973
라몽이 <rlatndus21049@gmail.com>
2026-02-18 12:44:01 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.45 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    let answer = Array(52).fill(0);
+    // console.log(answer)
+    
+    for (let i = 0; i < my_string.length; i++) {
+        const idx = alpha.indexOf(my_string[i]);
+        if (idx !== -1) {
+          answer[idx]++;
+        }
+    }
+    return answer;
+}
